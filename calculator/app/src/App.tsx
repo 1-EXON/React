382ec57df5a1d936049cd8f049d718438c87ce36
시혁 박 <exon.indiv@gmail.com>
2022-05-21 11:06:49 +0900

    Finish: calculator

--- calculator/app/src/App.tsx
+++ calculator/app/src/App.tsx
@@ -2,13 +2,29 @@ import React, { useState } from 'react'
 import './App.css'
 
 export default function App() {
-    const [x2, setX2] = useState<string>('')
-    const [x1, setX1] = useState<string>('')
-    const [n, setN] = useState<string>('')
+    const [numbers, setNumbers] = useState<{x2: string, x1: string, n: string}>({
+        x2: '',
+        x1: '',
+        n: ''
+    })
 
-    const onChangeX2 = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setX2(e.target.value)
-        console.log(x2)
+    const { x2, x1, n } = numbers
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target
+        setNumbers({
+            ...numbers,
+            [name]: value
+        })
+        console.log(numbers)
+        // (-b +- 루트(b^2 - 4ac))/2a
+    }
+
+    const onClick = (e: React.MouseEvent<HTMLElement>) => {
+        let arr: number[] = [x2, x1, n].map(number => parseInt(number));
+        const result1 = (arr[1] + Math.sqrt(arr[1]*arr[1] - (4 * arr[0] * arr[2]))) / (2 * arr[0])
+        const result2 = (arr[1] - Math.sqrt(arr[1]*arr[1] - (4 * arr[0] * arr[2]))) / (2 * arr[0])
+        alert(`${result1}, ${result2}`)
     }
 
     return (
@@ -18,22 +34,22 @@ export default function App() {
                 <span className="block text-sm font-medium text-slate-700">x^2 의 계수</span>
                 <div className="inline-block text-center mb-3">
                     <form className="inline-block text-center">
-                        <input type="text" value={x2} onChange={onChangeX2} className="mt-1 block w-50 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-slate-700 focus:ring-1 focus:ring-slate-700"></input>
+                        <input name="x2" type="text" value={x2} onChange={onChange} className="mt-1 block w-50 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-slate-700 focus:ring-1 focus:ring-slate-700"></input>
                     </form>
                 </div>
                 <span className="block text-sm font-medium text-slate-700">x 의 계수</span>
                 <div className="inline-block text-center mb-3">
                     <form className="inline-block text-center">
-                        <input type="text" value={x1} className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-slate-700 focus:ring-1 focus:ring-slate-700"></input>
+                        <input name="x1" type="text" value={x1} onChange={onChange} className="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-slate-700 focus:ring-1 focus:ring-slate-700"></input>
                     </form>
                 </div>
                 <span className="block text-sm font-medium text-slate-700">상수항</span>
                 <div className="inline-block text-center mb-3">
                     <form className="inline-block text-center">
-                        <input type="text" value={n} className="mt-1 block w-50 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-slate-700 focus:ring-1 focus:ring-slate-700"></input>
+                        <input name="n" type="text" value={n} onChange={onChange} className="mt-1 block w-50 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-slate-700 focus:ring-1 focus:ring-slate-700"></input>
                     </form>
                 </div>
-                <button className="bg-slate-700 hover:bg-slate-800 text-white font-bold py-2 px-4 border border-slate-700 rounded">
+                <button onClick={onClick} className="bg-slate-700 hover:bg-slate-800 text-white font-bold py-2 px-4 border border-slate-700 rounded">
                     계산하기
                 </button>
             </div>
